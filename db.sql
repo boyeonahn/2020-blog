8222e5288a520d477dcd75f643fd4b370e049a73
boyeonahn <boyeonahn@gmail.com>
2020-12-17 13:02:36 +0900

    data추가2

--- db.sql
+++ db.sql
@@ -1,59 +1,27 @@
-DROP DATABASE IF EXISTS mysqlutildemo;
-CREATE DATABASE mysqlutildemo;
-
-# DB 선택
-USE mysqlutildemo;
+# DB 생성
+DROP DATABASE IF EXISTS textBoard;
+CREATE DATABASE textBoard;
+USE textBoard;
 
 # 게시물 테이블 생성
 CREATE TABLE article (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regDate DATETIME NOT NULL,
     updateDate DATETIME NOT NULL,
-    title CHAR(100) NOT NULL,
-    `body` TEXT NOT NULL
+    title CHAR(200) NOT NULL,
+    `body` TEXT NOT NULL,
+    memberId INT(10) UNSIGNED NOT NULL,
+    boardId INT(10) UNSIGNED NOT NULL
 );
 
-# 회원 테이블 생성
-CREATE TABLE `member` (
-    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    regDate DATETIME NOT NULL,
-    updateDate DATETIME NOT NULL,
-    loginId CHAR(20) NOT NULL,
-    loginPw CHAR(100) NOT NULL,
-    `name` CHAR(200) NOT NULL
-);
-
-
-# 임시 회원
-INSERT INTO `member`
-SET regDate = NOW(),
-updateDate = NOW(),
-loginId = 'test1',
-loginPw = 'test1',
-`name` = '홍길동';
-
-# 임시 회원
-INSERT INTO `member`
-SET regDate = NOW(),
-updateDate = NOW(),
-loginId = 'test2',
-loginPw = 'test2',
-`name` = '홍길순';
-
-# 게시물 테이블에 memberId 칼럼 추가
-ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
-
-# 게시물 테이블에 hit 칼럼 추가
-ALTER TABLE article ADD COLUMN hit INT(10) UNSIGNED NOT NULL AFTER `body`;
-
-# 게시물 추가
+# 게시물 데이터 3개 생성
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = '제목1',
 `body` = '내용1',
 memberId = 1,
-hit = 5;
+boardId = 1;
 
 INSERT INTO article
 SET regDate = NOW(),
@@ -61,100 +29,97 @@ updateDate = NOW(),
 title = '제목2',
 `body` = '내용2',
 memberId = 1,
-hit = 4;
+boardId = 1;
 
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = '제목3',
 `body` = '내용3',
-memberId = 2,
-hit = 3;
+memberId = 1,
+boardId = 1;
 
-INSERT INTO article
+# 회원 테이블 생성
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(30) NOT NULL,
+    loginPw VARCHAR(50) NOT NULL,
+    `name` CHAR(30) NOT NULL
+);
+
+# 회원 데이터 생성
+INSERT INTO `member`
 SET regDate = NOW(),
 updateDate = NOW(),
-title = '제목4',
-`body` = '내용4',
-memberId = 2,
-hit = 20;
+loginId = 'test1',
+loginPw = 'test1',
+`name` = '테스터1';
 
-INSERT INTO article
+INSERT INTO `member`
 SET regDate = NOW(),
 updateDate = NOW(),
-title = '제목5',
-`body` = '내용5',
-memberId = 2,
-hit = 20;
+loginId = 'test2',
+loginPw = 'test2',
+`name` = '테스터2';
 
-INSERT INTO article
+# 게시판 테이블 생성
+CREATE TABLE board (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    `name` CHAR(20) NOT NULL,
+    `code` CHAR(20) NOT NULL
+);
+
+# 공지사항 게시판 추가
+INSERT INTO board 
 SET regDate = NOW(),
 updateDate = NOW(),
-title = '제목6',
-`body` = '내용6',
-memberId = 2,
-hit = 20;
+`name` = '공지사항',
+`code` = 'notice';
 
-INSERT INTO article
+# 자유 게시판 추가
+INSERT INTO board 
 SET regDate = NOW(),
 updateDate = NOW(),
-title = '제목7',
-`body` = '내용7',
-memberId = 2,
-hit = 20;
+`name` = '자유',
+`code` = 'free';
 
-INSERT INTO article
-SET regDate = NOW(),
-updateDate = NOW(),
-title = '제목8',
-`body` = '내용8',
-memberId = 2,
-hit = 20;
+UPDATE article
+SET boardId = 2
+LIMIT 2;
 
-INSERT INTO article
-SET regDate = NOW(),
-updateDate = NOW(),
-title = '제목9',
-`body` = '내용9',
-memberId = 2,
-hit = 20;
+# 각종 함수
+/*
+SELECT DATE(NOW());
+SELECT YEAR(NOW());
+SELECT MONTH(NOW());
+SELECT DAY(NOW());
+SELECT SUBSTR("안녕하세요.", 1, 2);
+SELECT SUBSTR("안녕하세요.", 2, 2);
+SELECT CONCAT("안녕", "하세요.");
+SELECT RAND() * 100;
+*/
 
-INSERT INTO article
-SET regDate = NOW(),
+# 게시물 랜덤 생성
+/*
+insert into article
+set regDate = NOW(),
 updateDate = NOW(),
-title = '제목10',
-`body` = '내용10',
-memberId = 2,
-hit = 20;
+title = concat("제목_", rand()),
+`body` = CONCAT("내용_", RAND()),
+memberId = FLOOR(RAND() * 2) + 1,
+boardId = FLOOR(RAND() * 2) + 1;
+*/
 
-INSERT INTO article
-SET regDate = NOW(),
-updateDate = NOW(),
-title = '제목11',
-`body` = '내용11',
-memberId = 2,
-hit = 20;
+# 3번글 내용을 마크다운 문법으로 수정
+UPDATE article SET `body` = '# 공지사항\r\n안녕하세요.\r\n이 사이트는 저의 글 연재 공간입니다.\r\n\r\n---\r\n\r\n# 이 사이트의 특징\r\n- A\r\n- B\r\n- C'
+WHERE id = '3';
 
-INSERT INTO article
-SET regDate = NOW(),
-updateDate = NOW(),
-title = '제목12',
-`body` = '내용12',
-memberId = 2,
-hit = 20;
+SELECT * FROM article;
 
-INSERT INTO article
-SET regDate = NOW(),
-updateDate = NOW(),
-title = '제목13',
-`body` = '내용13',
-memberId = 2,
-hit = 20;
-
-INSERT INTO article
-SET regDate = NOW(),
-updateDate = NOW(),
-title = '제목14',
-`body` = '내용14',
-memberId = 2,
-hit = 20;
+# 2번글 내용에 자바소스코드 넣기
+UPDATE article SET `body` = '# 자바기본문법\r\n```java\r\nint a = 10;\r\nint b = 20;\r\nint c = a + b;\r\n```'
+WHERE id = '2'; 
